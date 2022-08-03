c2bcf5ef235e1a3455479bb37c3d29829acc2bc3
TychoTa <tycho.tatitscheff+ananke@gmail.com>
2022-08-03 19:05:22 +0200

    modernize

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,6 +1,5 @@
 import React from "react";
 import PropTypes from "prop-types";
-
 export default function Task({
   task: { id, title, state },
   onArchiveTask,
@@ -8,48 +7,52 @@ export default function Task({
 }) {
   return (
     <div className={`list-item ${state}`}>
-      <label className="checkbox">
+      <label
+        htmlFor="checked"
+        aria-label={`archiveTask-${id}`}
+        className="checkbox"
+      >
         <input
           type="checkbox"
-          defaultChecked={state === "TASK_ARCHIVED"}
           disabled={true}
           name="checked"
+          id={`archiveTask-${id}`}
+          checked={state === "TASK_ARCHIVED"}
         />
         <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
       </label>
-      <div className="title">
+
+      <label htmlFor="title" aria-label={title} className="title">
         <input
           type="text"
           value={title}
           readOnly={true}
+          name="title"
           placeholder="Input title"
+          style={{ textOverflow: "ellipsis" }}
         />
-      </div>
+      </label>
 
-      <div className="actions" onClick={(event) => event.stopPropagation()}>
-        {state !== "TASK_ARCHIVED" && (
-          // eslint-disable-next-line jsx-a11y/anchor-is-valid
-          <a onClick={() => onPinTask(id)}>
-            <span className={`icon-star`} />
-          </a>
-        )}
-      </div>
+      {state !== "TASK_ARCHIVED" && (
+        <button
+          className="pin-button"
+          onClick={() => onPinTask(id)}
+          id={`pinTask-${id}`}
+          aria-label={`pinTask-${id}`}
+          key={`pinTask-${id}`}
+        >
+          <span className={`icon-star`} />
+        </button>
+      )}
     </div>
   );
 }
-
 Task.propTypes = {
-  /** Composition of the task */
   task: PropTypes.shape({
-    /** Id of the task */
     id: PropTypes.string.isRequired,
-    /** Title of the task */
     title: PropTypes.string.isRequired,
-    /** Current state of the task */
     state: PropTypes.string.isRequired,
   }),
-  /** Event to change the task to archived */
   onArchiveTask: PropTypes.func,
-  /** Event to change the task to pinned */
   onPinTask: PropTypes.func,
 };
--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -1,5 +1,3 @@
-// src/components/Task.stories.js
-
 import React from "react";
 
 import Task from "./Task";
@@ -17,7 +15,6 @@ Default.args = {
     id: "1",
     title: "Test Task",
     state: "TASK_INBOX",
-    updatedAt: new Date(2018, 0, 1, 9, 0),
   },
 };
 
@@ -34,5 +31,14 @@ Archived.args = {
   task: {
     ...Default.args.task,
     state: "TASK_ARCHIVED",
+  },
+};
+const longTitleString = `This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not!`;
+
+export const LongTitle = Template.bind({});
+LongTitle.args = {
+  task: {
+    ...Default.args.task,
+    title: longTitleString,
   },
 };
